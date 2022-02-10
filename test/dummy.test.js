144967b428327bf94afd29244c92d1e6e1ba0e46
jvwlehti <93128297+jvwlehti@users.noreply.github.com>
2022-02-10 12:51:01 +0200

    Update dummy.test.js

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -1,8 +1,8 @@
-const listHelper = require('../utils/list_helper').dummy
+const dummy = require('../utils/list_helper').dummy
 
 test('dummy returns on', () => {
     const blogs = []
 
-    const result = listHelper(blogs)
+    const result = dummy(blogs)
     expect(result).toBe(1)
 })
